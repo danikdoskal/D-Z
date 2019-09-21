9b85f715880b8c2d71c1f07c58bf1b2a2705b85a
danikdoskal <55136804+danikdoskal@users.noreply.github.com>
2019-09-21 22:52:19 +0700

    Create z4.py

---
+++ sweets/z4.py
@@ -0,0 +1,12 @@
+with open('input.txt', 'r') as f:
+    a,b,c = map(int,f.readlines())
+print(a,b,c)
+with open('output.txt','w') as f:
+    if ((a-b)%3) == 0:
+        f.write('3')
+    elif ((a-c)%3) == 0:
+        f.write('2')
+    else:
+        f.write('1')
+
+    
