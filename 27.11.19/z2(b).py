30d5deb9c09ad35dc62bc40c1d0eaee401b20df4
danikdoskal <55136804+danikdoskal@users.noreply.github.com>
2019-11-27 18:24:04 +0700

    Create z2(b).py

---
+++ 27.11.19/z2(b).py
@@ -0,0 +1,13 @@
+from random import *
+a=[]
+s=randint(1,10)
+#print(s)
+sp=[[randint(-100,100)for j in range(10)]for i in range(10)]
+#for i in range(10):
+#    for j in range(10):
+#        print(sp[i][j],end=' ')
+#    print()
+for i in range(0,10):
+    a.append(sp[i][s])
+#print(a)
+print(sum(a))
