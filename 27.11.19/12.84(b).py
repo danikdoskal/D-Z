eed1f92af78418cc6da6d07bd21faf178ef733b0
danikdoskal <55136804+danikdoskal@users.noreply.github.com>
2019-11-29 18:14:54 +0700

    Create 12.84(b).py

---
+++ 27.11.19/12.84(b).py
@@ -0,0 +1,12 @@
+from random import*
+sp=[[randint(-100,100)for j in range(10)]for i in range(10)]
+a=[]
+st=int(input('Введите номер столбца'))
+for i in range(10):
+    for j in range(10):
+        print(sp[i][j],end=' ')
+    print()
+for i in range(0,8):
+    a.append(sp[i][st-1])
+print(a)
+print(min(a))
